24b77bdb26d937fdb4d75add149bb2215475a983
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-22 17:26:44 -0500

    feat: :sparkles: Archivos helper necesarios

---
+++ API/Helpers/Authorization.cs
@@ -0,0 +1,11 @@
+namespace API.Helpers;
+
+public class Authorization
+{
+    public enum Roles
+    {
+        Administrator,
+        Empleado
+    }
+    public const Roles rol_default = Roles.Empleado;
+}
